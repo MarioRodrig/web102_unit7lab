725aeabd96ea5afbb59807f78af9705805da8742
Mario Rodriguez <m.rodrig.dev@gmail.com>
2026-04-19 00:47:15 -0400

    FIXED KEY

--- src/client.js
+++ src/client.js
@@ -2,5 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 const URL = 'https://ryigducrvbphhbfsqasg.supabase.co'
 
+const API_KEY = 'sb_publishable_9_6d1885MD7PoZzlpNV2WA_doCZK6wg'
 
 export const supabase = createClient(URL, API_KEY);
