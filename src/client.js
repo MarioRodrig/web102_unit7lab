75b102ab12c40b9b0918200451d9965d2c30e79a
Mario Rodriguez <m.rodrig.dev@gmail.com>
2026-04-19 00:44:37 -0400

    fixing issue with git key

--- src/client.js
+++ src/client.js
@@ -2,6 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 const URL = 'https://ryigducrvbphhbfsqasg.supabase.co'
 
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5aWdkdWNydmJwaGhiZnNxYXNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY1MzcyNjUsImV4cCI6MjA5MjExMzI2NX0.94oe4FgjRfOyTBTmyZDcBhbavL1AWI8zP_6cTiOeCvU'
 
 export const supabase = createClient(URL, API_KEY);
